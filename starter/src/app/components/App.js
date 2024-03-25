5a984a824602572d942354c209e92092beafa28d
aaron moroney <aaron.createdesign@gmail.com>
2024-03-25 22:11:42 +0000

    FEATURE: routing added

--- starter/src/app/components/App.js
+++ starter/src/app/components/App.js
@@ -1,51 +1,48 @@
-import { useState } from "react";
+import { Link, Route, Routes} from 'react-router-dom'
 
 import '../../App.css';
-import '../../widgets/Library/components/Library.js'
 import Library from '../../widgets/Library/components/Library.js';
+import Search from '../../features/search/components/Search.js';
 
 function App() {
-  const [showSearchPage, setShowSearchpage] = useState(false);
+  // const [showSearchPage, setShowSearchpage] = useState(false);
   return (
     <div className="app">
-      {/* search page */}
-      {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => setShowSearchpage(!showSearchPage)}
-            >
-              Close
-            </a>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                placeholder="Search by title, author, or ISBN"
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-        </div>
-      ) : (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <Library />
-            </div>
-          </div>
-        </div>
-      )}
+      <Routes>
+        <Route 
+          exact path='/' 
+            element={
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Library />
+                </div>
+              </div>
+            </div> 
+          }/>
+        <Route 
+          exact path='/search' 
+            element={
+              <Search />
+          }/>
+      </Routes>
       <div className="open-search">
-        <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
-      </div>
+        <Link
+          to="/search"
+        >
+        Add a book
+        </Link>
+      </div> 
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
