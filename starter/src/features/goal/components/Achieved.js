acca4022c1b9750581efccc116608ecfdf19572f
aaron moroney <aaron.createdesign@gmail.com>
2024-04-04 16:16:03 +0100

    FEATURE: Confetti on goal achieved!

---
+++ starter/src/features/goal/components/Achieved.js
@@ -0,0 +1,22 @@
+import Confetti from 'react-dom-confetti';
+
+const config = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 40,
+  elementCount: "175",
+  dragFriction: 0.12,
+  duration: 3000,
+  stagger: 3,
+  width: "10px",
+  height: "10px",
+  perspective: "500px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
+
+const Achieved = ({ achievedValue }) => {
+  console.log('achievedValue', achievedValue);
+  return <Confetti active={ achievedValue === 100 ? true : false } config={ config }/>
+}
+
+export default Achieved;
