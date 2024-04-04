7dfe35b9eb7fbc22f5baf6a13e1a4bc213ce61af
aaron moroney <aaron.createdesign@gmail.com>
2024-04-04 17:08:06 +0100

    Added helper function

--- starter/src/widgets/Goal/components/Goal.js
+++ starter/src/widgets/Goal/components/Goal.js
@@ -6,7 +6,7 @@ import { GoalModal } from '../../../features/goal';
 import { ProgressBar } from '../../../features/goal';
 import { GoalHeader } from '../../../features/goal';
 import { Achieved } from '../../../features/goal';
-import { getFromLocalStorage } from '../helpers/goal';
+import { getFromLocalStorage, calculateProgress } from '../helpers/goal';
 
 const Goal = () => {
     const [editing, setIsEditing] = useState(false);
@@ -14,11 +14,9 @@ const Goal = () => {
     const [achievedValue, setAchievedValue] = useState(0)
 
     const { shelfState} = useContext(SelectorContext);
-
-    const read = shelfState?.filter(book => book.shelf === 'read');
-    const value = bookGoal === 0 ? 0 : Math.min(100, Math.round(read.length / bookGoal * 100));
     
-    //move into helpers
+    const value = calculateProgress(shelfState, bookGoal);
+
     const  handleOpenEdit = () => {
         setIsEditing(true);
     };
